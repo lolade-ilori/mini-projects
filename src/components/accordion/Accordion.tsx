9a6f0d115fcc5b8ee178046e80da0440227bdf09
Lolade Ilori <loladeilori02@gmail.com>
2025-01-10 17:05:27 +0000

    accordion component done

--- src/components/accordion/Accordion.tsx
+++ src/components/accordion/Accordion.tsx
@@ -1,8 +1,71 @@
+import { useState } from "react";
+import data from "./data";
+import "./Accordion.scss";
 
 const Accordion = () => {
-  return (
-    <div>Accordion</div>
-  )
-}
+  const [selected, setSelected] = useState<number | null>(null);
+  const [enableMultiple, setEnableMultiple] = useState<boolean>(false);
+  const [multiple, setMultiple] = useState<number[]>([]);
 
-export default Accordion
+  const handleSingleSelection = (value: number) => {
+    setSelected(selected == value ? null : value);
+  };
+
+  const handleMultipleSelection = (value: number) => {
+    let cpyMultiple: number[] = [...multiple];
+
+    const currentIdIndex = cpyMultiple.indexOf(value);
+
+    if (currentIdIndex === -1) {
+      cpyMultiple.push(value);
+    } else cpyMultiple.splice(currentIdIndex, 1);
+
+    setMultiple(cpyMultiple);
+  };
+
+  return (
+    <div className="wrapper">
+      <button
+        className="multiple-enable"
+        onClick={() => setEnableMultiple(!enableMultiple)}
+      >
+        {enableMultiple ? "Disable Multi Selection" : "Enable Multi Selection"}
+      </button>
+
+      <div className="accordion">
+        {data &&
+          data.length > 0 &&
+          data.map((item) => (
+            <div key={item.id} className="accordion-box">
+              <div className="header">
+                <h3>{item.headerText}</h3>
+
+                <button
+                  onClick={
+                    enableMultiple
+                      ? () => handleMultipleSelection(item.id)
+                      : () => handleSingleSelection(item.id)
+                  }
+                >
+                  {selected === item.id ? "-" : "+"}
+                </button>
+              </div>
+              {enableMultiple
+                ? multiple.indexOf(item.id) !== -1 && (
+                    <div className="content">
+                      <p>{item.bodyText}</p>
+                    </div>
+                  )
+                : selected === item.id && (
+                    <div className="content">
+                      <p>{item.bodyText}</p>
+                    </div>
+                  )}
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default Accordion;
