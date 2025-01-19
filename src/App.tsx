c2b7c18c1d3f29a6dc05fe282f7833a885d9795c
Lolade Ilori <loladeilori02@gmail.com>
2025-01-19 23:35:32 +0000

    starting theme switch

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
 // import './App.css'
 import "./scss/main.scss";
 import { AppProvider } from "./context/AppContext";
-import QRGenerator from "./components/qrcode-generator/QRGenerator";
 
 function App() {
-  return (
-    <AppProvider>
-      <QRGenerator />
-    </AppProvider>
-  );
+  return <AppProvider></AppProvider>;
 }
 
 export default App;
